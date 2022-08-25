f5cac30fb6a36c85767d0f52e30fcfb29bd53907
PanosKontos <panos96el@gmail.com>
2022-08-25 23:50:49 +0300

    second

--- App.js
+++ App.js
@@ -9,12 +9,14 @@ export default function App() {
   }
 
 
+  {/* <Text style={styles.title}>Hello Panos!</Text> */}
   return (
     <View style={styles.appcontainer}>
-
-        <View >
-            <Text style={styles.title}>Hello Panos!</Text>
-      
+        <View style={styles.box1}>
+        </View>
+        <View style={styles.box2}>
+        </View>
+        <View style={styles.box3}>
         </View>
     
     </View>
@@ -25,11 +27,27 @@ const styles = StyleSheet.create({
   appcontainer: {
     height:'100%',
     backgroundColor: 'black',
-    alignItems: 'center',
+    alignItems: 'flex-end',
     justifyContent: 'center',
   },
   title: {
     color:'white',
     fontSize:24,
+  },
+
+  box1:{
+    backgroundColor:'darkblue',
+    height:40,
+    width:'50%',
+  },
+  box2:{
+    backgroundColor:'yellow',
+    height:40,
+    width:'50%',
+  },
+  box3:{
+    backgroundColor:'red',
+    height:40,
+    width:'50%',
   },
 });
