e405b8f527aeb29d696e2da733fceeeb95081bf4
PanosKontos <panos96el@gmail.com>
2022-08-25 23:36:44 +0300

    hey

--- App.js
+++ App.js
@@ -1,22 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+// import { StatusBar } from 'expo-status-bar';
+import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello Panos!</Text>
+  const onPressLearnMore = () =>{
+    alert("Click")
+  }
 
-      <StatusBar style="auto" />
+
+  return (
+    <View >
+        <View style={styles.appcontainer}>
+            <Text>Hello Panos!</Text>
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  appcontainer: {
+    backgroundColor: 'aqua',
     alignItems: 'center',
     justifyContent: 'center',
+    height:'100%',
   },
 });
